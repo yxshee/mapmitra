488d4bad683b7a4563738256ebfd04b9be289024
yash <yash999901@gmail.com>
2024-12-17 16:27:26 +0530

    Delete backend directory

--- backend/src/utils/AsyncHandler.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = (requestHandler) => {
-  return async (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
-      next(error)
-    );
-  };
-};
-
-export default asyncHandler;
